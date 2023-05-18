ddff5768d0543c3c7d992e78fc9378c88d3f9007
gayoung1115 <63089026+gayoung1115@users.noreply.github.com>
2023-05-19 00:13:22 +0900

    [D2] Title: 날짜 계산기, Time: 95 ms, Memory: 42,708 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -5,10 +5,14 @@ for time in range(1, T + 1):
     start_m, start_d, end_m, end_d = map(int, input().split())
     result = 0
 
-    result += dict_day[start_m] - start_d + 1
     if start_m < end_m:
+        result += dict_day[start_m] - start_d + 1
         for i in range(1, end_m - start_m):
             result += dict_day[start_m + i]
         result += end_d
+        
+    elif start_m == end_m:
+        result += end_d - start_d + 1
+
 
     print("#" + str(time) + " " + str(result))
